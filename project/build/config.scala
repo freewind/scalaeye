177282eda32b17ffe938296f753ea11d088ed218
nowind <nowind@192.168.0.3>
2011-02-23 13:49:03 +0800

    增加了一些常用库的引用

---
+++ project/build/config.scala
@@ -0,0 +1,26 @@
+import sbt._
+
+class ScalaeyeProject(info: ProjectInfo) extends DefaultWebProject(info) {
+
+	override val jettyPort = 8080
+
+	override def libraryDependencies = Set(
+		"org.slf4j" % "slf4j-api" % "1.6.1" % "compile->default",
+		"org.slf4j" % "slf4j-nop" % "1.6.1" % "compile->default",
+
+		"commons-fileupload" % "commons-fileupload" % "1.2.2" % "compile->default",
+		"commons-lang" % "commons-lang" % "2.6" % "compile->default",
+		"commons-io" % "commons-io" % "2.0.1" % "compile->default",
+		// "commons-codec" % "commons-codec" % "1.4" % "compile->default",
+		// "commons-collections" % "commons-collections" % "3.2.1" % "compile->default",
+
+		"org.mortbay.jetty" % "jetty" % "6.1.25" % "test->default",
+		"org.scalatest" % "scalatest" % "1.2" % "test->default",
+		"junit" % "junit" % "4.5" % "test->default",
+
+		"postgresql" % "postgresql" % "9.0-801.jdbc4" % "compile->default" // "p6spy" % "p6spy" % "1.3" % "test->default"
+	) ++ super.libraryDependencies
+
+	// to work with jrebel
+	override def scanDirectories = Nil
+}

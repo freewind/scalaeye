01226cafbddb3841b1449368751e1026f34b641e
nowind <nowind@192.168.0.3>
2011-03-07 21:26:35 +0800

    使用configgy作为配置文件的解析器

--- core/src/main/scala/app_config.scala
+++ core/src/main/scala/app_config.scala
@@ -1,14 +1,37 @@
 package org.scalaeye
 
-abstract class Mode
-case object dev extends Mode
-case object prod extends Mode
-case object test extends Mode
+import net.lag.configgy._
+import net.lag.logging.Logger
+import org.scalaeye._
 
-/** 程序配置类，TODO，请忽略 */
-object AppConfig {
+object AppConfig extends Init {
 
-	var port = 8080
-	var mode: Mode = dev
+	private var _mode: String = System.getProperty("mode") || "dev"
+	private var config: Config = _
+
+	def load(mode: String = this.mode) {
+		this._mode = mode
+		Configgy.configure(Context.classesDir / (mode+".conf"))
+		this.config = Configgy.config
+	}
+
+	def mode = _mode
+
+	override def init() {
+		this.load()
+	}
+
+	def getRoot: Config = config
+	def get(key: String): Option[ConfigMap] = config.getConfigMap(key)
+	def app: ConfigMap = { config.configMap("app") }
+	def db: ConfigMap = { config.configMap("db") }
+	def log: ConfigMap = { config.configMap("log") }
+	def plugins: ConfigMap = { config.configMap("plugins") }
+	def cache: ConfigMap = { config.configMap("cache") }
+	def mail: ConfigMap = { config.configMap("mail") }
+
+	def inDev: Boolean = this.mode == "dev"
+	def inTest: Boolean = this.mode == "test"
+	def inProd: Boolean = this.mode == "prod"
 
 }

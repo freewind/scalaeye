b7495b6a3b42eb2a35bada58779c6f81d4c6eee0
nowind <nowind@192.168.0.3>
2011-03-07 14:18:56 +0800

    增加了'aa := "AA"这种方式向context写入数据。增加了RichString

--- core/src/main/scala/package.scala
+++ core/src/main/scala/package.scala
@@ -4,12 +4,14 @@ import javax.servlet._, http._
 import scala.collection._
 import scala.collection.mutable.{ Map => MuMap }
 import scala.util.DynamicVariable
+import org.apache.commons.lang._
 
 package object scalaeye {
 
 	def context = Context.current
 
 	implicit def symbol2context(symbol: Symbol) = new SymbolContext(symbol)
+	implicit def string2rich(str: String) = new RichString(str)
 
 }
 
@@ -17,7 +19,13 @@ package scalaeye {
 
 	class SymbolContext(symbol: Symbol) {
 		def :=(value: Any) {
-			// context += (symbol.name -> value)
+			context(symbol.name) = value
 		}
+	}
+
+	class RichString(str: String) {
+		def isBlank = StringUtils.isBlank(str)
+		def isEmpty = StringUtils.isEmpty(str)
+		def whenNotBlank(block: String => Any): Any = if (!str.isBlank) { block(str) }
 	}
 }

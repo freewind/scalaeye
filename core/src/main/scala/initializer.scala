f1bdcb36af9d2164693a18ad6ca03e17cc532fc1
nowind <nowind@192.168.0.3>
2011-03-07 21:27:51 +0800

    重构了启动代码

--- core/src/main/scala/initializer.scala
+++ core/src/main/scala/initializer.scala
@@ -1,23 +1,35 @@
 package org.scalaeye
 
 import javax.servlet._
+import java.io.File
+import org.scalaeye._, mvc._
 
 /**
  * 当web server启动时，该类将被自动执行，可进行一些初始化工作
  *
  * 需要在web.xml中有以下配置（一定要放在最前面）
  *
+ * {{{
+ * <listener>
+ * <listener-class>org.scalaeye.Initializer</listener-class>
+ * </listener>
+ * }}}
  *
  */
-
 class Initializer extends ServletContextListener {
 
-	def contextDestroyed(event: ServletContextEvent) {
-
+	def contextInitialized(event: ServletContextEvent) {
+		Context.webappRoot = event.getServletContext.getRealPath("/")
+		val it = findSubclassesOf[Init].toList
+		it foreach { clsname =>
+			getObjectOrCreateInstanceOf[Init](clsname).init()
+		}
 	}
 
-	def contextInitialized(event: ServletContextEvent) {
-
+	def contextDestroyed(event: ServletContextEvent) {
 	}
 
 }
+
+/** 所有继承了该类的类，都将在web app启动时，被调用并执行init()方法 */
+trait Init { def init(): Any = {} }

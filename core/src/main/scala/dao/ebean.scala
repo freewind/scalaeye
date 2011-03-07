52a051dbbd22df3894fbc65ff0f57e64f65f235b
nowind <nowind@192.168.0.3>
2011-03-07 23:35:58 +0800

    为org.scalaeye.dao包增加注释

--- core/src/main/scala/dao/ebean.scala
+++ core/src/main/scala/dao/ebean.scala
@@ -2,10 +2,16 @@ package org.scalaeye.dao
 
 import com.avaje.ebean._
 
+/**
+ * 用于方便得到Ebean的server对象，该对象为线程安全的，可用于执行各种数据库操作
+ */
 trait EbeanServer {
-	def ebean = Ebean.getServer(null)
+	val ebean = Ebean.getServer(null)
 }
 
+/**
+ * 供model继承的类，提供了增删改等操作
+ */
 abstract class EbeanEntity extends EbeanServer {
 
 	def save(): Unit = ebean.save(this);
@@ -14,10 +20,14 @@ abstract class EbeanEntity extends EbeanServer {
 
 }
 
+/**
+ * 供dao继承的类，提供了一些常用的查询操作
+ */
 abstract class EbeanDao[T] extends EbeanServer {
 
 	type M[T] = Manifest[T]
 
+	/** 使用id来查询 */
 	def find(id: Any)(implicit m: M[T]): Option[T] = {
 		ebean.find(m.erasure, id) match {
 			case x: AnyRef => Some(x.asInstanceOf[T])
@@ -25,13 +35,15 @@ abstract class EbeanDao[T] extends EbeanServer {
 		}
 	}
 
+	/** 得到一个query对象*/
 	def find()(implicit m: M[T]): Query[T] = ebean.find(m.erasure).asInstanceOf[Query[T]]
 
-	def ref(id: Any)(implicit m: M[T]): Option[T] = {
-		ebean.getReference(m.erasure, id) match {
-			case x: AnyRef => Some(x.asInstanceOf[T])
-			case _ => None
-		}
-	}
+	/** TODO 抄来的函数，还不理解，先去掉 */
+	//	def ref(id: Any)(implicit m: M[T]): Option[T] = {
+	//		ebean.getReference(m.erasure, id) match {
+	//			case x: AnyRef => Some(x.asInstanceOf[T])
+	//			case _ => None
+	//		}
+	//	}
 
 }
--- core/src/main/scala/dao/package.scala
+++ core/src/main/scala/dao/package.scala
@@ -1,12 +1,17 @@
 package org.scalaeye
 
+/**
+ * 预定义了一些在dao中常用的类型
+ */
 package object dao {
 
+	// 提供以下这些类，主要是因为如果使用了java中的orm，如Ebean,hibernate,jpa之类，
+	// 在定义时必须使用java中的集合类
 	type JInteger = java.lang.Integer
 	type JLong = java.lang.Long
 	type JDouble = java.lang.Double
 	type JList[T] = java.util.List[T]
 	type JSet[T] = java.util.Set[T]
 	type JMap[K, V] = java.util.Map[K, V]
-}
 
+}
